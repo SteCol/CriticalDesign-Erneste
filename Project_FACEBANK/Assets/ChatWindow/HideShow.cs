70b89e16bf9df2bfb9bf802d0883f473ca0554e7
scolruyt <scolruyt@none.com>
2016-12-07 16:48:58 +0100

    Added first UI elements

---
+++ Project_FACEBANK/Assets/ChatWindow/HideShow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HideShow : MonoBehaviour {
+
+
+    public Vector3 startPos;
+    public Vector3 stopPos;
+    public float slerpValue;
+    public float slerpSpeed;
+
+    public bool show;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (show)
+            slerpValue = slerpValue + slerpSpeed * Time.deltaTime;
+            transform.position = Vector3.Slerp(startPos, stopPos, slerpValue);
+	}
+}
