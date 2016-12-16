93c66edde4d86aae2f79f6b231b6c2596ab5ae8a
Stef Colruyt <stef.col@hotmail.com>
2016-12-16 16:37:39 +0100

    Started second attempt at GetCharacters script

---
+++ Project_FACEBANK/Assets/Code/Characters/Periods/Period.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Period {
+    public string codeName;
+    public int periodNumber;
+    public List<Question> questions = new List<Question>();
+    public List<StatusUpdate> statusUpdates = new List<StatusUpdate>();
+}
